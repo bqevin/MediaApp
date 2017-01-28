5a10707d2d31d6933e9b39de2f4b29b8931eaea9
Kevin Barasa <kevin.barasa001@gmail.com>
2017-01-28 18:06:24 +0300

    Removed Video Manager layouts

--- app/src/main/java/com/projects/kevinbarassa/mediaapp/VideoFragment.java
+++ app/src/main/java/com/projects/kevinbarassa/mediaapp/VideoFragment.java
@@ -19,11 +19,13 @@ public class VideoFragment extends Fragment {
     }
 
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_videos, container, false);
     }
+
 
 }
