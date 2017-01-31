59ce41bf97982344e682e5baebf2c1e9e33968e1
Kevin Barasa <kevin.barasa001@gmail.com>
2017-01-31 18:46:06 +0300

    Fixed Fragments Transaction commits error

--- app/src/main/java/com/projects/kevinbarassa/mediaapp/MainActivity.java
+++ app/src/main/java/com/projects/kevinbarassa/mediaapp/MainActivity.java
@@ -4,12 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.TabLayout;
+
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,17 +16,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
 import com.projects.kevinbarassa.mediaapp.helper.SQLiteHandler;
 import com.projects.kevinbarassa.mediaapp.helper.SessionManager;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -78,6 +73,10 @@ public class MainActivity extends AppCompatActivity
 //        tabLayout = (TabLayout) findViewById(R.id.tabs);
 //        tabLayout.setupWithViewPager(viewPager);
 
+        final TopStoriesFragment topStory = new TopStoriesFragment();
+        final NearMeFragment nearMe = new NearMeFragment();
+        final SubscriptionsFragment subscription = new SubscriptionsFragment();
+
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
@@ -87,13 +86,13 @@ public class MainActivity extends AppCompatActivity
                     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                         switch (item.getItemId()) {
                             case R.id.action_trending:
-
+                                openFragment(topStory);
                                 break;
                             case R.id.action_nearme:
-
+                                openFragment(nearMe);
                                 break;
                             case R.id.action_subscription:
-
+                                openFragment(subscription);
                                 break;
                         }
                         return false;
@@ -119,6 +118,18 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+    }
+
+
+
+    //Open any Frag
+    private void openFragment(final Fragment fragment)   {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
 
     }
 
