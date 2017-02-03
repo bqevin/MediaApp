9df7645f5f7d1f1af21d8d092750097296d66650
Kevin Barasa <kevin.barasa001@gmail.com>
2017-02-03 11:25:57 +0300

    Videos section fully active AF!

--- app/src/main/java/com/projects/kevinbarassa/mediaapp/VideosActivity.java
+++ app/src/main/java/com/projects/kevinbarassa/mediaapp/VideosActivity.java
@@ -3,6 +3,8 @@ package com.projects.kevinbarassa.mediaapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,7 +16,21 @@ public class VideosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        //Avoid error of provision by WINDOW Decor
+        if (toolbar != null){
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setHomeButtonEnabled(true);
+        }
+
+        if (savedInstanceState == null) {
+            //Ensure no blank screen on launch
+            VideosFragment homeFragment = new VideosFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, homeFragment).commit();
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,4 +43,17 @@ public class VideosActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+
+
+    //Replace any Frag
+    public void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.fragment_container, fragment);
+
+        transaction.commit();
+    }
+
 }
+
+
